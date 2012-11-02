93b3637f51ef21008c5a91dc978026a7e9b9da71
Fredrik Jönsson <fjo@kth.se>
2012-11-02 12:18:05 +0100

    Clean out unused imports.

--- src/se/kth/webtex/WebTex.java
+++ src/se/kth/webtex/WebTex.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 import javax.servlet.ServletConfig;
